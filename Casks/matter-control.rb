0dfd38742ba6bc92dda09330a820dc2984c7a4d1
J. Russell Smyth <jrussell.smyth@gmail.com>
2019-05-03 21:47:19 -0700

    Add Matter Control

---
+++ Casks/matter-control.rb
@@ -0,0 +1,10 @@
+cask 'matter-control' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://mattercontrol.appspot.com/downloads/mattercontrol-mac-os-x/release'
+  name 'Matter Control'
+  homepage 'https://www.matterhackers.com/store/l/mattercontrol/sk/MKZGTDW6'
+
+  app 'MatterControl.app'
+end
