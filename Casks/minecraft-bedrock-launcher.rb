05f556b69fec9c818d8cb6535c3b8e7cd0840564
J. Russell Smyth <jrussell.smyth@gmail.com>
2019-05-03 20:28:54 -0700

    initial commit

---
+++ Casks/minecraft-bedrock-launcher.rb
@@ -0,0 +1,10 @@
+https://mrarm.io/r/mcpelauncher-osxcask 'microsoft-bedrock-edition' do
+  version :latest
+  sha256 :no_check
+
+  url 'https://mrarm.io/r/mcpelauncher-osx'
+  name 'Minecraft Bedrock Launcher for Mac'
+  homepage ''
+
+  app 'Minecraft Bedrock Launcher.app'
+end
