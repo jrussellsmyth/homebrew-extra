553d7f68d6414f806663325481123f4c8e6bf043
J. Russell Smyth <jrussell.smyth@gmail.com>
2019-05-22 00:10:10 -0700

    added sha and uninstall for zebralette

--- Casks/zebralette.rb
+++ Casks/zebralette.rb
@@ -1,12 +1,12 @@
 cask 'zebralette' do
-  version 'V3.0 REV 389'
-  sha256 :no_check
+  version '28 7422'
+  sha256 'e0ea90d0e63b0dbfa96096346bcbdfcbb0052e6947e57764cbaed6a45c19b99e'
 
   url 'https://uhedownloads-heckmannaudiogmb.netdna-ssl.com/releases/Zebra2_28_7422_Mac.zip'
   name 'Zebralette Synth'
   homepage 'https://u-he.com/products/zebralette/'
 
-  pkg 'Zebra2 2.8.0 Installer.pkg'
+  pkg 'Zebra2_7422_Mac/Zebra2 2.8.0 Installer.pkg'
 
-  uninstall pkgutil: ''
+  uninstall pkgutil: 'com\.u-he\.Zebra2\..*'
 end
