fc68c62a85584e98e14bcbe210808ef509ec105c
J. Russell Smyth <jrussell.smyth@gmail.com>
2019-05-25 11:09:24 -0700

    Add Soundly

---
+++ Casks/soundly.rb
@@ -0,0 +1,11 @@
+cask 'soundly' do
+  version  :latest
+  sha256 :no_check
+
+  url 'https://storage.googleapis.com/soundlyapp/Soundly.dmg'
+  name 'Soundly'
+  homepage 'http://getsoundly.com'
+
+  app 'Soundly.app'
+
+end
