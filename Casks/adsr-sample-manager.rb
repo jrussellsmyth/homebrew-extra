8ac5b273005ea4b346f6319d604c85f881762e13
J. Russell Smyth <jrussell.smyth@gmail.com>
2020-03-14 13:30:43 -0700

    updated adsr-sample-manager to 1.5.1

--- Casks/adsr-sample-manager.rb
+++ Casks/adsr-sample-manager.rb
@@ -1,12 +1,12 @@
 cask 'adsr-sample-manager' do
-  version '1.2.0' 
-  sha256 '8764706c2ca857f4afb6505cea8d5063e20ba26b97a545921dec1a6635c34598' 
+  version '1.5.1' 
+  sha256 '7d3fe4a516497b42b83b1a458161fa461545b3f0fd67bd43ca952a09cd50def5' 
 
-  url "http://d1oiqz8fobe593.cloudfront.net/Plug-ins/ADSR/Sample%20Manager/MAC%20ADSR_Sample_Manager.#{version}.zip?AWSAccessKeyId=AKIAI22Z7KGZ6SFG5ZOQ&Expires=1560215550&Signature=ANLtwhqTFp7%2BFsayltX3QV%2BOfHE%3D"
+  url "http://d1oiqz8fobe593.cloudfront.net/Plug-ins/ADSR/Sample%20Manager/MAC%20ADSR_Sample_Manager.1.5.1.zip?AWSAccessKeyId=AKIAI22Z7KGZ6SFG5ZOQ&Expires=1584217700&Signature=ZMLMMDyMTvB0R%2BS7%2Fp2Kf16%2BLrg%3D"
   name 'ADSR Sample Manager'
   homepage 'https://www.adsrsounds.com/product/software/adsr-sample-manager/'
 
-  pkg 'ADSR Sample Manager 1.2.0.pkg'
+  pkg 'ADSR Sample Manager 1.5.1.pkg'
 
   uninstall pkgutil: 'com\.adsr\.samplemanager\..*'
 end
